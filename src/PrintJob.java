035b6317c4aa286e756972ed5d77528cdd73e336
talktogandhali <talktogandhali@gmail.com>
2018-06-04 16:53:51 +1000

    Added cost property
    Change-Id: Ida7af7b3cac183423169c1876d66dcdabd5bdf25
    Signed-off-by: talktogandhali <talktogandhali@gmail.com>

--- src/PrintJob.java
+++ src/PrintJob.java
@@ -1,14 +1,66 @@
 
 public class PrintJob {
-	 int numberOfPages;
-	 int numberofColouredPages;
-	 boolean doubleSided;
-	 int numberofBWPages;
+	private int numberOfPages;
+	private int numberofColouredPages;
+	private boolean doubleSided;
+	private int numberofBWPages;
+	private double cost;
 	 
-	 public PrintJob(int numberofPages, int numberofColouredPages, boolean doubleSided) {
+	 public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+
+	public int getNumberofColouredPages() {
+		return numberofColouredPages;
+	}
+
+
+	public void setNumberofColouredPages(int numberofColouredPages) {
+		this.numberofColouredPages = numberofColouredPages;
+	}
+
+
+	public boolean isDoubleSided() {
+		return doubleSided;
+	}
+
+
+	public void setDoubleSided(boolean doubleSided) {
+		this.doubleSided = doubleSided;
+	}
+
+
+	public int getNumberofBWPages() {
+		return numberofBWPages;
+	}
+
+
+	public void setNumberofBWPages(int numberofBWPages) {
+		this.numberofBWPages = numberofBWPages;
+	}
+
+
+	public double getCost() {
+		return cost;
+	}
+
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+	
+
+	public PrintJob(int numberofPages, int numberofColouredPages, boolean doubleSided) {
 		 this.numberOfPages = numberofPages;
 		 this.numberofColouredPages = numberofColouredPages;
 		 this.numberofBWPages = numberofPages - numberofColouredPages;
 		 this.doubleSided = doubleSided;
+		 this.cost = 0;
 	 }
 }
